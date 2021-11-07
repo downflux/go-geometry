65ac6d75a4cfced9b39477e2adc26c35ce9ecf39
Minke Zhang <mzhang@blogzhang.com>
2021-11-07 15:51:39 -0800

    Add segment.S struct

---
+++ segment/segment.go
@@ -0,0 +1,36 @@
+package segment
+
+import (
+	"github.com/downflux/go-geometry/line"
+	"github.com/downflux/go-geometry/vector"
+)
+
+type S struct {
+	l   line.L
+	min float64
+	max float64
+}
+
+func New(l line.L, min float64, max float64) *S {
+	return &S{
+		l:   l,
+		min: min,
+		max: max,
+	}
+}
+
+func (s S) L() line.L     { return s.l }
+func (s S) TMin() float64 { return s.min }
+func (s S) TMax() float64 { return s.max }
+
+func (s S) Project(v vector.V) float64 {
+	t := s.l.Project(v)
+	if t < s.TMin() {
+		return s.TMin()
+	} else if t > s.TMax() {
+		return s.TMax()
+	}
+	return t
+}
+
+func (s S) Feasible() bool { return s.min <= s.max }
