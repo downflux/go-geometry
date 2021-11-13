c287962ccacd86447452f829a8cf18e86644934a
Minke Zhang <mzhang@blogzhang.com>
2021-11-12 17:11:20 -0800

    Intermediate commit adding hyperplane and hyperspace

--- plane/plane.go
+++ plane/plane.go
@@ -1,74 +1,183 @@
-// Package plane defines a geometric half-plane.
+// Plane defines a parametric N-dimensional geometric plane.
 package plane
 
 import (
-	"github.com/downflux/go-geometry/line"
-	"github.com/downflux/go-geometry/vector/v2d"
+	// "math"
+
+	// "github.com/downflux/go-geometry/circle"
+	// "github.com/downflux/go-geometry/epsilon"
+	"github.com/downflux/go-geometry/vector"
 )
 
-// HP defines a half-plane, geometrically consisting of a normal vector n to the
-// plane, and a point p which defines the origin of n.
+// P represents an N-dimensional plane embedded into a possibly higher
+// dimensonal space. When N = 1, the "plane" is a line of the form
 //
-// N.B.: By arbitrary convention, vectors pointing away from N are not
-// permissible within the half-plane.
-type HP struct {
-	l line.L
+//   P := C + tD
+//
+// Where C is some offset vector, and D is the single directional basis of the
+// ilne.
+type P struct {
+	// basis is a list of vectors whose span define the plane. The basis
+	// does not have to be orthonormal.
+	//
+	// The number of elements in the basis represent the dimension of the
+	// plane.
+	basis []vector.V
+
+	// c represents the offset of the plane in higher-dimensional space.
+	c vector.V
 }
 
-// New constructs a half-plane passing through a point P and with normal N.
-func New(p vector.V, n vector.V) *HP {
-	// D returns the characteristic line along the plane is bisected. Points
-	// to the "left" of the line are not permissible.
-	//
-	// N.B.: RVO2 defines the "right" side of the line as non-permissible,
-	// but we have considered an anti-clockwise rotation of N() (e.g. +X to
-	// +Y) to be more natural. See
-	// https://github.com/snape/RVO2/blob/57098835aa27dda6d00c43fc0800f621724884cc/src/Agent.cpp#L314
-	// for evidence of this distinction.
-	d := *vector.New(-n.Y(), n.X())
+func New(c vector.V, basis []vector.V) *P { return &P{c: c, basis: basis} }
 
-	return &HP{
-		l: *line.New(p, d),
+func (pl P) Basis() []vector.V { return pl.basis }
+func (pl P) D() vector.D       { return vector.D(len(pl.Basis())) }
+func (pl P) C() vector.V       { return pl.c }
+
+// T returns the N-dimensional projected point of the plane, given a parametric
+// vector input. The dimension of the parametric vector must match the dimension
+// of the plane.
+func (pl P) T(t vector.V) vector.V {
+	var r vector.V
+
+	for d, b := range pl.Basis() {
+		r = vector.Add(r, vector.Add(pl.C(), vector.Scale(t.X(vector.D(d)), b)))
 	}
+	return r
 }
 
-func (hp HP) L() line.L { return hp.l }
+// Project takes as input a higher-dimensional point, and returns a parametric
+// vector representing the point on the plane closest to the input.
+func (pl P) Project(v vector.V) vector.V {
+	xs := make([]float64, len(pl.Basis()))
 
-func (hp HP) D() vector.V { return hp.l.D() }
-func (hp HP) P() vector.V { return hp.l.P() }
-
-// N returns the normal vector of the plane, pointing away from the invalid
-// region.
-func (hp HP) N() vector.V { return *vector.New(hp.D().Y(), -hp.D().X()) }
-
-// In checks if a given point in vector space is in valid region of the
-// half-plane.
-func (hp HP) In(p vector.V) bool {
-	// Generate a vector with tail on D and pointing towards the input.
-	v := vector.Sub(p, hp.P())
-
-	// Check relative orientation between w and D.
-	//
-	// Remember that by the right hand rule, if v is on the "left" of the
-	// plane,
-	//
-	//   D x v > 0, and
-	//   N • v > 0
-	//
-	// As the left half of the plane is considered invalid, we are looking
-	// instead for the complementary result.
-	return vector.Determinant(hp.D(), v) <= 0
+	for d, b := range pl.Basis() {
+		xs[d] = vector.Dot(b, vector.Sub(v, pl.C()))
+	}
+	return *vector.New(xs...)
 }
 
-// Disjoint returns if the region of interection between two planes is empty.
+/*
+// L defines a parametric line of the form
 //
-// Disjoint checks if the characteristic lines of the two planes are parallel,
-// and if the a line drawn between two points on the planes, away from the first
-// plane, lie in the feasible region of the first plane.
-func Disjoint(a HP, b HP) bool {
-	return vector.Within(a.N(), vector.Scale(-1, b.N())) && !a.In(vector.Sub(b.P(), a.P()))
+//   L := P + tD
+type L struct {
+	p vector.V
+	d vector.V
 }
 
-func Within(a HP, b HP) bool {
-	return vector.Within(a.N(), b.N()) && vector.Within(a.P(), b.P())
+func New(p vector.V, d vector.V) *L { return &L{p: p, d: d} }
+
+func (l L) P() vector.V { return l.p }
+func (l L) D() vector.V { return l.d }
+
+// T calculates the vector value on the line which corresponds to the input
+// parametric t-value.
+func (l L) T(t float64) vector.V { return vector.Add(l.p, vector.Scale(t, l.d)) }
+
+// Project calculates the projected t-value of v onto l by finding the point on
+// L closest to v.
+func (l L) Project(v vector.V) float64 {
+	return vector.Dot(l.D(), vector.Sub(v, l.P()))
 }
+
+// Intersect returns the intersection point between two lines.
+//
+// Returns error if the lines are parallel.
+//
+// Find the intersection between the two lines as a function of the constraint
+// parameter t.
+//
+// Given two constraints L, M, we need to find their intersection; WLOG, let's
+// project the intersection point onto L.
+//
+// We know the parametric equation form of these lines -- that is,
+//
+//   L = P + tD
+//   M = Q + uE
+//
+// At their intersection, we know that L meets M:
+//
+//   L = M
+//   => P + tD = Q + uE
+//
+// We want to find the projection onto L, which means we need to find a concrete
+// value for t. the other parameter u doesn't matter so much -- let's try to get
+// rid of it.
+//
+//   uE = P - Q + tD
+//
+// Here, we know P, D, Q, and E are vectors, and we can decompose these into a
+// system of equations by isolating their orthogonal (e.g. horizontal and
+// vertical) components.
+//
+//   uEx = Px - Qx + tDx
+//   uEy = Py - Qy + tDy
+//
+// Solving for u, we get
+//
+//   (Px - Qx + tDx) / Ex = (Py - Qy + tDy) / Ey
+//   => Ey (Px - Qx + tDx) = Ex (Py - Qy + tDy)
+//
+// We leave the task of simplifying the above terms as an exercise to the
+// reader. Isolating t, and noting some common substitutions, we get
+//
+//   t = || E x (P - Q) || / || D x E ||
+//
+// See https://gamedev.stackexchange.com/a/44733 for more information.
+func (l L) Intersect(m L) (vector.V, bool) {
+	d := vector.Determinant(l.D(), m.D())
+	n := vector.Determinant(m.D(), vector.Sub(l.P(), m.P()))
+
+	if epsilon.Within(d, 0) {
+		return vector.V{}, false
+	}
+
+	return l.T(n / d), true
+}
+
+// IntersectCircle returns the intersection points between a line and a circle.
+// If the line does not intersect a circle, the function will return not
+// successful.
+//
+// As a line stretches to infinity in both directions, it is not possible for a
+// line to intersect the circle partway.
+//
+// If the line lies tangent to the circle, then the returned t-values are the
+// same.
+//
+// See https://stackoverflow.com/a/1084899 for more information.
+func (l L) IntersectCircle(c circle.C) (vector.V, vector.V, bool) {
+	p := vector.Sub(l.P(), c.P())
+
+	dot := vector.Dot(p, l.D())
+	discriminant := dot*dot + c.R()*c.R() - vector.SquaredMagnitude(p)
+
+	// The line does not intersect the circle.
+	if discriminant < 0 {
+		return vector.V{}, vector.V{}, false
+	}
+
+	// Find two intersections between line and circle. This is equivalent to
+	// having two additional constraints which lie tangent to the circle at
+	// these two points.
+	tl := -dot - math.Sqrt(discriminant)
+	tr := -dot + math.Sqrt(discriminant)
+
+	return l.T(tl), l.T(tr), true
+}
+
+// Distance finds the distance between the line l and a point p.
+//
+// The distance from a line L to a point Q is given by
+//
+//   d := || D x (Q - P) || / || D ||
+//
+// See
+// https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Another_vector_formulation
+// for more information.
+func (l L) Distance(p vector.V) float64 {
+	v := vector.Sub(p, l.P())
+	return math.Abs(vector.Determinant(l.D(), v) / vector.Magnitude(l.D()))
+}
+*/
--- plane/plane_test.go
+++ plane/plane_test.go
@@ -3,175 +3,202 @@ package plane
 import (
 	"testing"
 
+	"github.com/downflux/go-geometry/circle"
+	"github.com/downflux/go-geometry/epsilon"
 	"github.com/downflux/go-geometry/vector/v2d"
 )
 
-func TestIn(t *testing.T) {
-	type check struct {
-		v    vector.V
-		want bool
-	}
+func TestDistance(t *testing.T) {
 	testConfigs := []struct {
-		name  string
-		hp    HP
-		d     vector.V
-		tests []check
+		name string
+		l    L
+		p    vector.V
+		want float64
 	}{
 		{
-			name: "Vertical",
-			hp: *New(
-				*vector.New(0, 0),
-				*vector.New(1, 0),
-			),
-			d: *vector.New(0, 1),
-			tests: []check{
-				{v: *vector.New(0, 0), want: true},
-				{v: *vector.New(1, 0), want: true},
-				{v: *vector.New(-1, 0), want: false},
-			},
+			name: "Trivial",
+			l:    L{p: *vector.New(0, 0), d: *vector.New(0, 1)},
+			p:    *vector.New(0, 0),
+			want: 0,
 		},
 		{
-			name: "Horizontal",
-			hp: *New(
-				*vector.New(0, 0),
-				*vector.New(0, 1),
-			),
-			d: *vector.New(-1, 0),
-			tests: []check{
-				{v: *vector.New(0, 0), want: true},
-				{v: *vector.New(0, 1), want: true},
-				{v: *vector.New(0, -1), want: false},
-			},
+			name: "SimpleUnitDirection",
+			l:    L{p: *vector.New(0, 0), d: *vector.New(0, 1)},
+			p:    *vector.New(1, 1),
+			want: 1,
 		},
 		{
-			name: "Sloped",
-			hp: *New(
-				*vector.New(0, 0),
-				*vector.New(1, 1),
-			),
-			d: *vector.New(-1, 1),
-			tests: []check{
-				{v: *vector.New(0, 0), want: true},
-				{v: *vector.New(1, 1), want: true},
-				{v: *vector.New(-1, -1), want: false},
-			},
-		},
-		{
-			name: "SlopedOffset",
-			hp: *New(
-				*vector.New(0, 1),
-				*vector.New(1, 1),
-			),
-			d: *vector.New(-1, 1),
-			tests: []check{
-				{v: *vector.New(0, 0), want: false},
-				{v: *vector.New(1, 1), want: true},
-				{v: *vector.New(2, 2), want: true},
-			},
+			name: "SimpleLargeDirection",
+			l:    L{p: *vector.New(0, 0), d: *vector.New(0, 100)},
+			p:    *vector.New(1, 1),
+			want: 1,
 		},
 	}
 
 	for _, c := range testConfigs {
 		t.Run(c.name, func(t *testing.T) {
-			for _, test := range c.tests {
-				if got := c.hp.D(); !vector.Within(got, c.d) {
-					t.Fatalf("D() = %v, want = %v", got, c.d)
-				}
-				if got := c.hp.In(test.v); got != test.want {
-					t.Errorf("In(%v) = %v, want = %v", test.v, got, test.want)
-				}
+			if got := c.l.Distance(c.p); !epsilon.Within(c.want, got) {
+				t.Errorf("Distance() = %v, want = %v", got, c.want)
 			}
 		})
 	}
 }
 
-func TestDisjoint(t *testing.T) {
+func TestT(t *testing.T) {
 	testConfigs := []struct {
 		name string
-		a    HP
-		b    HP
-		want bool
+		l    L
+		t    float64
+		want vector.V
 	}{
 		{
-			name: "Parallel",
-			a: *New(
-				*vector.New(0, 1),
-				*vector.New(0, 1),
-			),
-			b: *New(
-				*vector.New(0, -1),
-				*vector.New(0, 1),
-			),
-			want: false,
+			name: "SimpleHorizontal",
+			l:    L{p: *vector.New(0, 1), d: *vector.New(1, 0)},
+			t:    1,
+			want: *vector.New(1, 1),
 		},
 		{
-			name: "Parallel/OrderInvariance",
-			a: *New(
-				*vector.New(0, -1),
-				*vector.New(0, 1),
-			),
-			b: *New(
-				*vector.New(0, 1),
-				*vector.New(0, 1),
-			),
-			want: false,
-		},
-
-		{
-			name: "AntiParallel/Facing",
-			a: *New(
-				*vector.New(0, 1),
-				*vector.New(0, -1),
-			),
-			b: *New(
-				*vector.New(0, -1),
-				*vector.New(0, 1),
-			),
-			want: false,
-		},
-		{
-			name: "AntiParallel/Facing/OrderInvariance",
-			a: *New(
-				*vector.New(0, -1),
-				*vector.New(0, 1),
-			),
-			b: *New(
-				*vector.New(0, 1),
-				*vector.New(0, -1),
-			),
-			want: false,
-		},
-
-		{
-			name: "AntiParallel/Away/OrderInvariance",
-			a: *New(
-				*vector.New(0, 1),
-				*vector.New(0, 1),
-			),
-			b: *New(
-				*vector.New(0, -1),
-				*vector.New(0, -1),
-			),
-			want: true,
-		},
-		{
-			name: "AntiParallel/Away/OrderInvariance",
-			a: *New(
-				*vector.New(0, -1),
-				*vector.New(0, -1),
-			),
-			b: *New(
-				*vector.New(0, 1),
-				*vector.New(0, 1),
-			),
-			want: true,
+			name: "SimpleVertical",
+			l:    L{p: *vector.New(1, 0), d: *vector.New(0, 1)},
+			t:    1,
+			want: *vector.New(1, 1),
 		},
 	}
 
 	for _, c := range testConfigs {
 		t.Run(c.name, func(t *testing.T) {
-			if got := Disjoint(c.a, c.b); got != c.want {
-				t.Errorf("Disjoint() = %v, want = %v", got, c.want)
+			if got := c.l.T(c.t); !vector.Within(got, c.want) {
+				t.Errorf("T() = %v, want = %v", got, c.want)
+			}
+		})
+	}
+}
+
+func TestProject(t *testing.T) {
+	testConfigs := []struct {
+		name string
+		l    L
+		v    vector.V
+		want float64
+	}{
+		{
+			name: "OnLine/SimpleHorizontal",
+			l:    L{p: *vector.New(0, 1), d: *vector.New(1, 0)},
+			v:    *vector.New(1, 1),
+			want: 1,
+		},
+		{
+			name: "OnLine/SimpleVertical",
+			l:    L{p: *vector.New(1, 0), d: *vector.New(0, 1)},
+			v:    *vector.New(1, 1),
+			want: 1,
+		},
+
+		{
+			name: "OffLine/SimpleHorizontal",
+			l:    L{p: *vector.New(0, 1), d: *vector.New(1, 0)},
+			v:    *vector.New(0, 0),
+			want: 0,
+		},
+	}
+
+	for _, c := range testConfigs {
+		t.Run(c.name, func(t *testing.T) {
+			if got := c.l.Project(c.v); !epsilon.Within(got, c.want) {
+				t.Errorf("Project() = %v, want = %v", got, c.want)
+			}
+		})
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	testConfigs := []struct {
+		name    string
+		l       L
+		m       L
+		success bool
+		want    vector.V
+	}{
+		{
+			name:    "SimpleOrigin",
+			l:       L{p: *vector.New(0, 0), d: *vector.New(1, 0)},
+			m:       L{p: *vector.New(0, 0), d: *vector.New(0, 1)},
+			success: true,
+			want:    *vector.New(0, 0),
+		},
+		{
+			name:    "SimpleYIntercept",
+			l:       L{p: *vector.New(0, 0), d: *vector.New(1, 0)},
+			m:       L{p: *vector.New(1, 0), d: *vector.New(0, 1)},
+			success: true,
+			want:    *vector.New(1, 0),
+		},
+	}
+
+	for _, c := range testConfigs {
+		t.Run(c.name, func(t *testing.T) {
+			if got, ok := c.l.Intersect(c.m); ok != c.success || !vector.Within(got, c.want) {
+				t.Errorf("Intersect() = %v, %v, want = %v, %v", got, ok, c.want, c.success)
+			}
+		})
+	}
+}
+
+func TestIntersectionCircle(t *testing.T) {
+	testConfigs := []struct {
+		name    string
+		l       L
+		c       circle.C
+		success bool
+		want    []vector.V
+	}{
+		{
+			name:    "SimpleOriginIntersection",
+			l:       L{p: *vector.New(0, 0), d: *vector.New(1, 0)},
+			c:       *circle.New(*vector.New(0, 0), 1),
+			success: true,
+			want: []vector.V{
+				*vector.New(-1, 0),
+				*vector.New(1, 0),
+			},
+		},
+		{
+			name:    "SimpleNoIntersection",
+			l:       L{p: *vector.New(0, 0), d: *vector.New(1, 0)},
+			c:       *circle.New(*vector.New(0, 2), 1),
+			success: false,
+			want: []vector.V{
+				*vector.New(0, 0),
+				*vector.New(0, 0),
+			},
+		},
+		{
+			name:    "SimpleTangent",
+			l:       L{p: *vector.New(0, 0), d: *vector.New(1, 0)},
+			c:       *circle.New(*vector.New(0, 1), 1),
+			success: true,
+			want: []vector.V{
+				*vector.New(0, 0),
+				*vector.New(0, 0),
+			},
+		},
+		{
+			name:    "OffCenterLineIntersect",
+			l:       L{p: *vector.New(0, 1), d: *vector.New(1, 0)},
+			c:       *circle.New(*vector.New(0, 0), 1),
+			success: true,
+			want: []vector.V{
+				*vector.New(0, 1),
+				*vector.New(0, 1),
+			},
+		},
+	}
+
+	for _, c := range testConfigs {
+		t.Run(c.name, func(t *testing.T) {
+			vl, vr, ok := c.l.IntersectCircle(c.c)
+			if ok != c.success || !vector.Within(vl, c.want[0]) || !vector.Within(vr, c.want[1]) {
+				t.Fatalf("IntersectCircle() = %v, %v, %v, want = %v, %v, %v", vl, vr, ok, c.want[0], c.want[1], c.success)
 			}
 		})
 	}
