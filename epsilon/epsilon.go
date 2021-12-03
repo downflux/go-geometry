2581054a427481605b891fa3afb60af78b66344c
Minke Zhang <minke.zhang@gmail.com>
2021-12-02 17:13:23 -0800

    Fix float comparison for infinities

--- epsilon/epsilon.go
+++ epsilon/epsilon.go
@@ -9,5 +9,8 @@ const (
 )
 
 func Within(a float64, b float64) bool {
+	if (a == math.Inf(-1) && b == math.Inf(-1)) || (a == math.Inf(0) && b == math.Inf(0)) {
+		return true
+	}
 	return math.Abs(a-b) < epsilon
 }
---
+++ epsilon/epsilon_test.go
@@ -0,0 +1,56 @@
+package epsilon
+
+import (
+	"math"
+	"testing"
+)
+
+func TestWithin(t *testing.T) {
+	testConfigs := []struct {
+		name string
+		a    float64
+		b    float64
+		want bool
+	}{
+		{
+			name: "Infinity/Positive",
+			a:    math.Inf(0),
+			b:    math.Inf(0),
+			want: true,
+		},
+		{
+			name: "Infinity/Negative",
+			a:    math.Inf(-1),
+			b:    math.Inf(-1),
+			want: true,
+		},
+
+		{
+			name: "Infinity/NotEqual",
+			a:    math.Inf(-1),
+			b:    math.Inf(0),
+			want: false,
+		},
+
+		{
+			name: "Finite/Equal",
+			a:    1.2,
+			b:    1.2,
+			want: true,
+		},
+		{
+			name: "Finite/NotEqual",
+			a:    1.2,
+			b:    1.1,
+			want: false,
+		},
+	}
+
+	for _, c := range testConfigs {
+		t.Run(c.name, func(t *testing.T) {
+			if got := Within(c.a, c.b); got != c.want {
+				t.Errorf("Within() = %v, want = %v", got, c.want)
+			}
+		})
+	}
+}
